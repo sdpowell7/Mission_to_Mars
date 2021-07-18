4ef37dee82a9de477859f42a671b77f504dd8235
sdunderhill <81715217+sdunderhill@users.noreply.github.com>
2021-07-18 13:17:56 -0500

    Add files via upload

--- Mission-to-Mars/app.py
+++ Mission-to-Mars/app.py
@@ -10,7 +10,7 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mong.db.mars.find_one()
+    mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
